28f95ce2ca3bbe77522f953915eb7d32a1c0dedc
artem_balykov <artem.balikov@yandex.ru>
2024-01-22 18:14:43 +0700

    set non block socket

--- server.cpp
+++ server.cpp
@@ -26,7 +26,11 @@ int setNonblock(int fd) {
 #if defined (O_NONBLOCK)
     if (-1 == (flags = fcntl(fd, F_GETFL, 0)))
         flags = 0;
-    return fcntl(fd, F_SETFL, 0)
+    return fcntl(fd, F_SETFL, flags|O_NONBLOCK);
+#else
+    flags = 1;
+    return ioctl(fd, FIOBIO, &flags);
+#endif
 }
 
 int main(int argc, char** argv)
