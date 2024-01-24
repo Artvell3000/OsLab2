c80228cfcd3546446cae979cf91c7a4455d01b76
artem_balykov <artem.balikov@yandex.ru>
2024-01-24 13:14:28 +0700

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -40,9 +40,9 @@ int main(int argv, char** argc)
 
     
     while (1) {
-        char* buf = randStr(20);
+        char buf[] = randStr(20);
 
-        int sendResult = send(socketClient, *buf, sizeof(buf), 0);
+        int sendResult = send(socketClient, buf, sizeof(buf), 0);
         if (sendResult < 0) {
             perror("send!!!");
         }
